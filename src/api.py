da6f88cb884e1cd5b0512f2668a14bb2be89a29b
dkolas <4643013+dkolas@users.noreply.github.com>
2023-01-25 16:49:00 -0500

    Changes to bring param descriptions into code

--- src/api.py
+++ src/api.py
@@ -2,6 +2,7 @@
 from typing import Any, Dict, List, Optional, Type
 
 import openai
+from pydantic import Field
 from steamship import Steamship, Tag
 from steamship.data import GenerationTag, TagKind, TagValueKey
 from steamship.invocable import Config, InvocableResponse, InvocationContext
@@ -20,17 +21,17 @@ class PromptGenerationPlugin(Tagger):
     """
 
     class PromptGenerationPluginConfig(Config):
-        openai_api_key: str
-        max_words: int
-        model: Optional[str] = "text-davinci-002"
-        temperature: Optional[float] = 0.4
-        top_p: Optional[int] = 1
-        n_completions: Optional[int] = 1
-        echo: Optional[bool] = False
-        stop: Optional[List[str]] = None
-        presence_penalty: Optional[int] = 0
-        frequency_penalty: Optional[int] = 0
-        best_of: Optional[int] = 1
+        openai_api_key: str = Field("", description="An openAI API key to use. If left default, will use Steamship's API key.")
+        max_words: int = Field(description="The maximum number of words to generate per request")
+        model: Optional[str] = Field("text-davinci-002", description="The OpenAI model to use.  Can be a pre-existing fine-tuned model.")
+        temperature: Optional[float] = Field(0.4, description="Controls randomness. Lower values produce higher likelihood / more predictable results; higher values produce more variety. Values between 0-1.")
+        top_p: Optional[int] = Field(1, description="Controls the nucleus sampling, where the model considers the results of the tokens with top_p probability mass. Values between 0-1.")
+        n_completions: Optional[int] = Field(1, description="How many completions to generate for each prompt.")
+        echo: Optional[bool] = Field(False, description= "Echo back the prompt in addition to the completion")
+        stop: Optional[str] = Field("", description="Up to 4 sequences where the API will stop generating further tokens. The returned text will not contain the stop sequence. Value is comma separated string version of the sequence.")
+        presence_penalty: Optional[int] = Field(0, description="Control how likely the model will reuse words. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics. Number between -2.0 and 2.0.")
+        frequency_penalty: Optional[int] = Field(0, description="Control how likely the model will reuse words. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim. Number between -2.0 and 2.0.")
+        best_of: Optional[int] = Field(1, description="Generates best_of completions server-side and returns the \"best\" (the one with the highest log probability per token).")
 
         def __init__(self, **kwargs):
             kwargs["stop"] = (
@@ -40,8 +41,9 @@ class PromptGenerationPlugin(Tagger):
             )
             super().__init__(**kwargs)
 
-    def config_cls(self) -> Type[Config]:
-        return self.PromptGenerationPluginConfig
+    @classmethod
+    def config_cls(cls) -> Type[Config]:
+        return cls.PromptGenerationPluginConfig
 
     config: PromptGenerationPluginConfig
 
